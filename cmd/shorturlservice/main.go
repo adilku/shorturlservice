3686d6039d0ef05a3cb3f7d61e1946d9806fd953
adilku <adhan2a@gmail.com>
2021-10-22 11:51:01 +0300

    upd config

--- cmd/shorturlservice/main.go
+++ cmd/shorturlservice/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/adilku/shorturlservice/internal/app/shorturlserver"
 	"log"
@@ -13,12 +12,11 @@ var (
 )
 
 func init() {
-	flag.StringVar(&configPath, "config-path", "configs/walletserver.toml", "path to config file")
+	flag.StringVar(&configPath, "config-path", "configs/shorturlservice.toml", "path to config file")
 }
 
 func main() {
 	flag.Parse()
-	fmt.Println(configPath)
 	config := shorturlserver.NewConfig()
 	_, err := toml.DecodeFile(configPath, config)
 	if err != nil {
